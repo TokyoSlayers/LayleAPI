eb33507c8c455a5b8968bc70d10d724b3682c21c
Asus-tuf <therualaz.max@hotmail.fr>
2021-02-17 11:34:30 +0100

    add and remove host perms

---
+++ src/main/java/net/TokyoSlayer/LayleAPI/Finish/Event/PlayerAddToHostEvent.java
@@ -0,0 +1,32 @@
+package net.TokyoSlayer.LayleAPI.Finish.Event;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.bukkit.permissions.PermissionAttachment;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class PlayerAddToHostEvent extends Event {
+
+    private static final HandlerList HANDLERS_LIST = new HandlerList();
+    HashMap<UUID, PermissionAttachment> perms = new HashMap<>();
+
+    public PlayerAddToHostEvent(Player player, Plugin plugin) {
+        if(player.hasPermission("HubLayle.host") && player.getServer().getOnlinePlayers().size() ==0){
+            PermissionAttachment attachment = player.addAttachment(plugin);
+            perms.put(player.getUniqueId(), attachment);
+            PermissionAttachment pperms = perms.get(player.getUniqueId());
+            pperms.setPermission("HubLayle.host.ok", true);
+        }
+    }
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS_LIST;
+    }
+}
